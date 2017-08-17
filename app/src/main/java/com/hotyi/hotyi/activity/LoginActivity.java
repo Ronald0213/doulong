6ee7edfa5d5207208b59eaa796fb7a6b447010e1
Ronald0213 <30928379+Ronald0213@users.noreply.github.com>
2017-08-17 15:43:12 +0800

    0817——test

--- app/src/main/java/com/hotyi/hotyi/activity/LoginActivity.java
+++ app/src/main/java/com/hotyi/hotyi/activity/LoginActivity.java
@@ -1,16 +1,79 @@
 package com.hotyi.hotyi.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.hotyi.hotyi.R;
 
-public class LoginActivity extends Activity {
+import org.w3c.dom.Text;
+
+public class LoginActivity extends Activity implements View.OnClickListener {
+    private String password;
+    private String phone_num;
+    private Button btn_login;
+    private TextView text_forget_password;
+    private TextView text_regist;
+    private ImageButton imgbtn_qq;
+    private ImageButton imgbtn_weixin;
+    private ImageButton imgbtn_weibo;
+    private EditText edt_phone_num;
+    private EditText edt_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    public void initView() {
+        btn_login = (Button) findViewById(R.id.btn_login);
+        text_forget_password = (TextView) findViewById(R.id.text_forget_password);
+        text_regist = (TextView) findViewById(R.id.text_regist);
+        edt_password = (EditText) findViewById(R.id.edt_password);
+        edt_phone_num = (EditText) findViewById(R.id.edt_phone_num);
+        imgbtn_qq = (ImageButton) findViewById(R.id.imgbtn_qq);
+        imgbtn_weibo = (ImageButton) findViewById(R.id.imgbtn_weibo);
+        imgbtn_weixin = (ImageButton) findViewById(R.id.imgbtn_weixin);
+
+        btn_login.setOnClickListener(this);
+        text_regist.setOnClickListener(this);
+        text_forget_password.setOnClickListener(this);
+        imgbtn_qq.setOnClickListener(this);
+        imgbtn_weibo.setOnClickListener(this);
+        imgbtn_weixin.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                phone_num = edt_phone_num.getText().toString();
+                password = edt_password.getText().toString();
+
+
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+                break;
+            case R.id.text_forget_password:
+                break;
+            case R.id.text_regist:
+                break;
+            case R.id.imgbtn_qq:
+                break;
+            case R.id.imgbtn_weibo:
+                break;
+            case R.id.imgbtn_weixin:
+                break;
+        }
     }
 }
