09a8870a5274adcd4b9461b293276fb037a3d398
Ronald0213 <30928379+Ronald0213@users.noreply.github.com>
2017-08-22 19:21:48 +0800

    Ronald---regist begin.

--- app/src/main/java/com/hotyi/hotyi/activity/RegistActivity.java
+++ app/src/main/java/com/hotyi/hotyi/activity/RegistActivity.java
@@ -1,16 +1,84 @@
 package com.hotyi.hotyi.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.hotyi.hotyi.R;
 
-public class RegistActivity extends Activity {
+public class RegistActivity extends Activity implements View.OnClickListener{
 
+    private ImageButton imageButton_back;
+    private EditText editText_phone;
+    private Button button_next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+        initView();
+    }
+    public void initView(){
+        imageButton_back = (ImageButton)findViewById(R.id.regist_btn_back);
+        editText_phone = (EditText)findViewById(R.id.regist_edit_phone);
+        button_next = (Button)findViewById(R.id.regist_btn_next);
+
+        imageButton_back.setOnClickListener(this);
+        button_next.setOnClickListener(this);
+
+        editText_phone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.regist_btn_back:
+                setResult(20);
+                finish();
+                break;
+            case R.id.regist_btn_next:
+                String phone_num = editText_phone.getText().toString();
+                int len = phone_num.length();
+                if(len < 11){
+                    Toast.makeText(RegistActivity.this,"请输入正确的手机号码",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent =  new Intent(RegistActivity.this,RegistNextActivity.class);
+                intent.putExtra("phone",phone_num);
+                startActivityForResult(intent,25);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 25){
+
+        }
     }
 }
--- app/src/main/java/com/hotyi/hotyi/activity/RegistNextActivity.java
+++ app/src/main/java/com/hotyi/hotyi/activity/RegistNextActivity.java
@@ -1,16 +1,118 @@
 package com.hotyi.hotyi.activity;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.hotyi.hotyi.R;
+import com.hotyi.hotyi.other.HotyiClass.LoginData;
+import com.hotyi.hotyi.other.HotyiClass.LoginInfo;
+import com.hotyi.hotyi.utils.HotyiHttpConnection;
+import com.hotyi.hotyi.utils.HttpException;
+import com.hotyi.hotyi.utils.MyAsynctask;
+import com.hotyi.hotyi.utils.async.OnDataListener;
 
-public class RegistNextActivity extends Activity {
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.HashMap;
+
+import io.rong.imkit.RongIM;
+import io.rong.imlib.RongIMClient;
+
+public class RegistNextActivity extends MyBaseActivity implements OnDataListener {
+
+    private String phone_num = null;
+    private static final int REGIST = 25;
+    Handler myHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            }
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist_next);
+        Intent intent = getIntent();
+        phone_num = intent.getStringExtra("phone");
+        mAsyncTaskManager.request(REGIST, true, this);
+    }
+
+
+
+    @Override
+    public Object doInBackground(int requestCode, String parameter) throws HttpException {
+        switch (requestCode){
+            case REGIST:
+                try {
+
+                    String login_url = MyAsynctask.HOST + MyAsynctask.SendRegistCode;
+                    URL url = new URL(login_url);
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put("PhoneNum", phone_num);
+                    if(phone_num == null){
+                        Log.e("RSA",".......");
+                    }
+                    else
+                        Log.e("RSA","----   "+phone_num.toString());
+                    Log.e("RSA","++++   "+phone_num);
+                    return HotyiHttpConnection.getInstance(getApplicationContext()).post(map, url);
+//                    return HotyiHttpConnection.getInstance(getApplicationContext()).post();
+                } catch (Exception e) {
+
+                }
+                break;
+        }
+        return null;
+    }
+
+    @Override
+    public void onSuccess(int requestCode, Object result) {
+        super.onSuccess(requestCode, result);
+        switch (requestCode){
+            case REGIST:
+                try {
+                    String str = result.toString();
+                    JSONObject jsonObject = new JSONObject(str);
+                    final LoginInfo myLoginInfo = new LoginInfo();
+                    myLoginInfo.setCode(Integer.valueOf(jsonObject.get("code").toString()));
+                    myLoginInfo.setResult_msg(jsonObject.get("result_msg").toString());
+                    myLoginInfo.setReturn_msg(jsonObject.get("retrun_msg").toString());
+                    JSONObject logindataJson = new JSONObject(jsonObject.get("data").toString());
+                    if (myLoginInfo.getCode() == -1){
+                        Toast.makeText(RegistNextActivity.this,"Test",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    LoginData loginData = new LoginData();
+                    loginData.setRYToken(logindataJson.getString("RYToken").toString());
+                    loginData.setNickName(logindataJson.get("NickName").toString());
+                    loginData.setHeadImageUrl(logindataJson.get("HeadImage").toString());
+                    loginData.setAccountInfo(logindataJson.get("AccountInformation").toString());
+                    myLoginInfo.setLoginData(loginData);
+                    if (myLoginInfo.getCode() == 1) {
+                        String token = myLoginInfo.getLoginData().getRYToken();
+                        Log.e("login",token);
+
+                    }
+                }catch (Exception e){
+                    Log.e("LOGIN","  " +e.toString());
+                }
+                break;
+
+
+        }
+    }
+
+    @Override
+    public void onFailure(int requestCode, int state, Object result) {
+        super.onFailure(requestCode, state, result);
     }
 }
